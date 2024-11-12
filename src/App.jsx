fa6304f55886b96a8681067577281ee8861035de
Daniel Kim <118289602+danyobosayo@users.noreply.github.com>
2024-11-12 01:57:06 -0600

    App Component with Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import Navbar from './components/Navbar'
-import FeaturedItem from './components/FeaturedItem'
-import React from 'react'
+import Navbar from "./components/Navbar";
+import FeaturedItem from "./components/FeaturedItem";
+import Dashboard from "./components/Dashboard";
+import React from "react";
 
 function App() {
   return (
     <div>
       <Navbar />
       <FeaturedItem />
+      <Dashboard />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
