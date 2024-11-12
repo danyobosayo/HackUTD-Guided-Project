ca239e7117be67aa9fdbd96238de3a854861f9a5
Daniel Kim <118289602+danyobosayo@users.noreply.github.com>
2024-11-11 23:35:22 -0600

    Guided Track Daniel Version

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar'
+import FeaturedItem from './components/FeaturedItem'
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <FeaturedItem />
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+      extend: {
+          colors: {
+            pastelblue: '#A1CAF1',
+            pastelgreen: '#B0E57C',
+            pastelpink: '#FDC1C5',
+            darkgreen: '#355E3B', 
+            darkblue: '#003366',
+          },
+      },
+      fontFamily: {
+        'Poppins': ['Poppins', 'sans-serif'],
+        'Inter': ['Inter', 'sans-serif'],
+      }
+  },
+  plugins: [],
+};
