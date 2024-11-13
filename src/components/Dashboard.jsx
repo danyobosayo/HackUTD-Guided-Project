bc1d14706cda935e463864c559f1779921d33541
Daniel Kim <118289602+danyobosayo@users.noreply.github.com>
2024-11-13 00:27:49 -0600

    Feat: Dashboard stripped

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,12 @@
+// the data import statement allows us to access the json file we downloaded earlier. you can modify the content of the data such as different names, items, and pictures. 
+// But make sure you understand how the key-value structure of this document works. Altering the keys will change how we 'access' the data.
+
 import { useState } from 'react';
-import data from '../data/data.json';
+import data from '../data/data.json'; 
 import Card from './Card';
 
 function Dashboard() {
-    // State to store items, initialized with data from JSON file
-    const [items, setItems] = useState(data);
-
+    // Insert state to store items, initialized with data from JSON file
     return (
         <div className="relative my-12 flex flex-col items-center">
             {/* TITLE */}
@@ -14,17 +15,13 @@ function Dashboard() {
 
             {/* Event List Grid */}
             <div className="relative z-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {/* Map through items and render a Card component for each */}
-                {items.map((item, index) => (
-                    <Card
-                        key={index}
-                        name={item.name}
-                        img={item.img}
-                        date={item.date}
-                        status={item.status}
-                        bringing={item.bringing}
-                    />
-                ))}
+                {/* Map through items and render a Card component for each in the section below */}
+                <Card 
+                img='/images/sample1.webp'
+                name='Alan Roybal'
+                bringing='Fruit'
+                status='Going'
+                />
             </div>
         </div>
     );
