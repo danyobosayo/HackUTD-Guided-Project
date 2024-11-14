58c7b5d8bb46a1e42085b84372f488e6b383fdbd
Daniel Kim <118289602+danyobosayo@users.noreply.github.com>
2024-11-14 14:22:19 -0600

    Feat: Installed axios and started weather API

--- src/pages/ActivityPage.jsx
+++ src/pages/ActivityPage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function ActivityPage() {
   return (
-    <div>ActivityPage</div>
-  )
+    <div>
+      <div className="relative my-12 flex flex-col items-center">
+        {/* TITLE */}
+        <h1 className="text-4xl poppins-bold text-darkgreen">Weather</h1>
+        <hr className="mb-6 mt-4 w-1/3 border-pastelgreen" />
+
+        {/* Center Card for Vote and Weather */}
+        <div className="relative z-10 p-6 w-full max-w-2xl flex flex-col gap-6">
+          {/* COMPONENTS HERE */}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ActivityPage
+export default ActivityPage;
---
+++ src/utils/WeatherComponent.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { getFirestore, doc, getDoc, updateDoc } from 'firebase/firestore';
+import axios from 'axios';
+
+function WeatherComponent() {
+    return (
+        <div></div>
+    );
+}
+
+export default WeatherComponent;
