e2d690176e554384123cb3f431eaef41fefad471
Daniel Kim <118289602+danyobosayo@users.noreply.github.com>
2024-11-16 01:37:11 -0600

    Fix: Populated authContext.jsx file

--- src/contexts/authContext.jsx
+++ src/contexts/authContext.jsx
@@ -1,16 +1,35 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
-import { auth } from '../firebase/firebase';
+import { createContext, useContext, useEffect, useState } from "react";
+import {
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../firebase/firebase";
 
 const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
 
-return (
-    <div>
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user); // Ensure 'user' state is updated
+    });
+    return unsubscribe;
+  }, []);
 
-    </div>
+  const login = (email, password) =>
+    signInWithEmailAndPassword(auth, email, password);
+  const register = (email, password) =>
+    createUserWithEmailAndPassword(auth, email, password);
+  const logout = () => signOut(auth);
+
+  return (
+    <AuthContext.Provider value={{ user, login, register, logout }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function LandingPage() {
+    console.log('we in hre')
   return (
-	  <div>LandingPage</div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="">Welcome to Ripple & Refresh</h1>
+      <p className="text-lg text-darkblue mb-6">Please log in or register to continue</p>
+      <div className="flex space-x-4">
+        <Link to="/login" className="px-6 py-3">
+          Login
+        </Link>
+        <Link to="/register" className="px-6 py-3">
+          Register
+        </Link>
+      </div>
+    </div>
   );
 }
 
